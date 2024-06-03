6ad5d92775d11dc64cfbfc344cf8e077491248a7
anapeff <anais.pefferkorn@outlook.fr>
2024-06-03 09:17:56 +0200

    Correction mois diapo + message formulaire

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -12,9 +12,10 @@ const Form = ({ onSuccess, onError }) => {
     async (evt) => {
       evt.preventDefault();
       setSending(true);
-      // We try to call mockContactApi
+          // We try to call mockContactApi
       try {
         await mockContactApi();
+        onSuccess();
         setSending(false);
       } catch (err) {
         setSending(false);
